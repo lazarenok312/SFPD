9b668177060d21dd5d89c0e588125149b79e363d
Лазарев Дмитрий <69201114+lazarenok312@users.noreply.github.com>
2024-07-23 00:01:02 +0600

    сброс паролей

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,30 @@
 from django.urls import path
 from profiles import views
 from .views import *
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='user_login'),
     path('logout/', views.user_logout, name='user_logout'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
+
     path('profile/<slug:slug>/', ProfileDetailView.as_view(), name='profile_detail'),
     path('support/', views.SupportView.as_view(), name='support'),
     path('profiles_list/', profile_list, name='profile_list'),
